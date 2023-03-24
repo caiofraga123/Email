905e62fdf85fb7be56007b744c7d47bf6595ff6c
20211tiimi0010 <caiofcintra@gmail.com>
2023-03-24 10:03:31 -0300

    Projeto inteiro atualizado

--- app/src/main/java/cintra/coelho/fraga/caio/email/MainActivity.java
+++ app/src/main/java/cintra/coelho/fraga/caio/email/MainActivity.java
@@ -22,25 +22,32 @@ public class MainActivity extends AppCompatActivity {
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Obtendo dados digitados pelo usuário
+                // Obtendo o email digitado pelo usuário
                 EditText etEmail = (EditText) findViewById(R.id.etEmail);
                 String email = etEmail.getText().toString();
 
+                //Obtendo o assunto do email digitado pelo usuário
                 EditText etAssunto = (EditText) findViewById(R.id.etAssunto);
                 String assunto = etAssunto.getText().toString();
 
+                //Obtendo o texto do email digitado pelo usuário
                 EditText etTexto = (EditText) findViewById(R.id.etTexto);
                 String texto = etTexto.getText().toString();
 
+                /*Declarando uma intenção implícita que buscará os apps que conseguem
+                realizar a action SEND_TO*/
                 Intent i = new Intent(Intent.ACTION_SENDTO);
 
+                //Definindo que as apps que serão buscadas trabalham com envio e recebimento de email
                 i.setData(Uri.parse("mailto:"));
 
+                /*Preenchendo o intent com os dados do email para que ele seja enviado para a app externa*/
                 String[] emails = new String[]{email};
                 i.putExtra(Intent.EXTRA_EMAIL, emails);
                 i.putExtra(Intent.EXTRA_SUBJECT, assunto);
                 i.putExtra(Intent.EXTRA_TEXT, texto);
 
+                //Tentando executar o intent, caso ele não execute será mostrado um aviso para ele
                 try{
                     startActivity(Intent.createChooser(i, "Escolha o APP"));
                 }
